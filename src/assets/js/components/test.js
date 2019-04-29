860ac67a5bf35a3243c4e2c72413f13353e51d27
melnychuk yurii <uronon23@gmail.com>
2019-04-29 15:59:52 +0300

    refactoring main and test files

--- src/assets/js/components/test.js
+++ src/assets/js/components/test.js
@@ -3,7 +3,7 @@ $(function () {
     console.log("js working?");
 });
 
-
+//site navigation
 $('.header-btn').on('click', function() {
     $('html,body').animate({scrollTop:$('.delivery-form-wrap').offset().top+"px"},{duration:1E3});
 });
@@ -11,22 +11,22 @@ $('.header-btn').on('click', function() {
 $('.service-btn').on('click', function() {
     $('html,body').animate({scrollTop:$('.price-img').offset().top+"px"},{duration:1E3});
 });
-
+//site navigation end
 
 //images-zoom
-$(document).ready(function() { // Ждём загрузки страницы
-    $(".image").click(function(){	// Событие клика на маленькое изображение
-        var img = $(this);	// Получаем изображение, на которое кликнули
-        var src = img.attr('src'); // Достаем из этого изображения путь до картинки
-        $("body").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
-            "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
-            "<img src='"+src+"' class='popup_img' />"+ // Само увеличенное фото
+$(document).ready(function() {
+    $(".image").click(function(){
+        var img = $(this);
+        var src = img.attr('src');
+        $("body").append("<div class='popup'>"+
+            "<div class='popup_bg'></div>"+
+            "<img src='"+src+"' class='popup_img' />"+
             "</div>");
-        $(".popup").fadeIn(200); // Медленно выводим изображение
-        $(".popup_bg").click(function(){	// Событие клика на затемненный фон
-            $(".popup").fadeOut(200);	// Медленно убираем всплывающее окн
-            setTimeout(function() {	// Выставляем таймер
-                $(".popup").remove(); // Удаляем разметку всплывающего окна
+        $(".popup").fadeIn(200);
+        $(".popup_bg").click(function(){
+            $(".popup").fadeOut(200);
+            setTimeout(function() {
+                $(".popup").remove();
             }, 200);
         });
     });
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,2 @@
 
-
 //= components/test.js
